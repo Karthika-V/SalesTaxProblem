ea970b6dcba377afe399a056f2d5b2ed18ac6026
karthika <karthika.vsdvn@gmail.com>
2015-08-24 19:00:36 +0530

    fifth commit

--- src/salesBill.java
+++ src/salesBill.java
@@ -6,26 +6,30 @@ import java.util.Scanner;
 public class salesBill {
     public static void main(String args[])
     {
-        Scanner scan = new Scanner(System.in);
+        taxExempted(true, 12.49,findSalesTax(false));
+
+
         //Input
+        Scanner scan = new Scanner(System.in);
         String[] inp = new String[10];
         System.out.println("Enter the no. of products:");
         int num=scan.nextInt();
-        for(int i=0;i<num;i++) {
-
+        for(int i=0;i<num;i++)
             inp[i]=scan.nextLine();
-        }
+
 
         //gettingItemsPrice
         String noOfItems;
         String itemName;
         String price;
+        System.out.println(inp[0]);
         for(int i=0;i<num;i++) {
-            //System.out.println(inp[i]);
-            String[] parts = inp[i].split(" ");
+            System.out.println(i);
+            String[] parts = inp[i].split("at");
+
             noOfItems = parts[0];
-            //itemName = parts[1];
-            System.out.println(noOfItems+" "+itemName);
+
+            System.out.println(noOfItems+" ");
 
                     //+" "+parts[1]+":"+parts[2]);
             /*noOfItems = parts[0]; // 1
@@ -42,10 +46,7 @@ public class salesBill {
         System.out.println(noOfItems+" "+itemName+":"+price);
 
 
-        //findSalesTax
 
-        //goodsCategories
-        //taxExempted
         int taxExempted;
         if(taxExempted==1)
         {
@@ -57,5 +58,24 @@ public class salesBill {
 1 music CD at 14.99
 1 chocolate bar at 0.85
         */
+    }
+
+    private static double findSalesTax(boolean imported) {
+        double price=0.0;
+        double salesTax;
+        double totalPrice=0.0;
+        if(imported==true)
+            salesTax=0.15* price;
+        else
+            salesTax = 0.1 * price;
+        return salesTax;
+    }
+
+    private static void taxExempted(boolean taxExempted, double price, double salesTax) {
+        double totalPrice;
+        if(taxExempted==true)
+            totalPrice=price;
+        else
+            totalPrice=salesTax+price;
     }
 }
