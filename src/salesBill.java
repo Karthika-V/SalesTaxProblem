2cdb2608cb73532f1dae7c5cd5055272647484b9
karthika <karthika.vsdvn@gmail.com>
2015-08-24 17:50:31 +0530

    second commit

--- src/salesBill.java
+++ src/salesBill.java
@@ -4,9 +4,24 @@
 public class salesBill {
     public static void main(String args[])
     {
+        //billFormat
+        int noOfItems;
+        String itemName;
+        double price;
+        System.out.println(noOfItems+" "+itemName+":" price);
+
+
         //findSalesTax
+        //goodsCategories
+        //taxExempted
+        int taxExempted;
+        if(taxExempted==1)
+        {
+
+        }
+
 
         System.out.println("Enter the products number:");
-        System.out.println("1.Book/n2.Music CD/n");
+        System.out.println("1.Book 2.Music CD 3.Chocolate bar 4.Imported");
     }
 }
