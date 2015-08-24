f72c64856a01e46c16b39f466df6a8bba2984974
karthika <karthika.vsdvn@gmail.com>
2015-08-24 17:35:29 +0530

    first commit

---
+++ src/salesBill.java
@@ -0,0 +1,12 @@
+/**
+ * Created by karthika on 24/08/15.
+ */
+public class salesBill {
+    public static void main(String args[])
+    {
+        //findSalesTax
+
+        System.out.println("Enter the products number:");
+        System.out.println("1.Book/n2.Music CD/n");
+    }
+}
